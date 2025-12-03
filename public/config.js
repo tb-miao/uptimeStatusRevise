6f2deefe9e864d489bc6c265913e14d3ff53dd73
tb-miao <2307057760@qq.com>
2025-12-03 12:27:47 +0800

    重写

---
+++ public/config.js
@@ -0,0 +1,25 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'AUNya 状态监控',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur3107094-1e45d5a46802c7d5613c893d',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'AUNyaの小窝',
+      url: 'https://tbmiao.dpdns.org/'
+    },
+  ],
+};
